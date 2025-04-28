e10e6bfc73921a8b293365381984f00f6ee92916
Tanishq84 <tanishqsom_19@outlook.com>
2025-04-29 03:44:18 +0530

    Updated Description for Meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: "Permutes",
-  description: "Crafting Innovation Beyond Limits...",
+  description: "A research-driven engineering company specializing in drone technology, AI innovation, robotics development, aerospace research, and scientific problem-solving. We blend creativity, design, and cutting-edge technology to deliver pioneering solutions in autonomous systems, fluid dynamics, space exploration, and machine learning. Explore how Permutes is shaping the future through interdisciplinary engineering and innovation.",
 };
 
 export default function RootLayout({
