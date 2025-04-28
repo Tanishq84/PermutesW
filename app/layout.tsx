eeb220651442bb5123f2677518ceb47a884591a7
Tanishq84 <tanishqsom_19@outlook.com>
2025-04-29 03:46:36 +0530

    Fixed Theme Issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import StarBackground from "@/components/main/StarBackground";
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Permutes",
+  title: "Permutes: Crafting Innovation Beyond Limits",
   description: "A research-driven engineering company specializing in drone technology, AI innovation, robotics development, aerospace research, and scientific problem-solving. We blend creativity, design, and cutting-edge technology to deliver pioneering solutions in autonomous systems, fluid dynamics, space exploration, and machine learning. Explore how Permutes is shaping the future through interdisciplinary engineering and innovation.",
 };
 
