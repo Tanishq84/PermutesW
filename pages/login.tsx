cec684504a81c02e1443e23dcb11a2206770e9a2
Akshat Bisht <akshatyogeshbisht@gmail.com>
2025-05-02 03:41:44 +0530

    Clerk Login Integration

--- pages/login.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-import LoginPage from '@/components/bid_build/LoginPage';
-
-const Login = () => {
-  return <LoginPage />;
-};
-
-export default Login;
---
+++ pages/login/[[...index]].tsx
@@ -0,0 +1,8 @@
+// pages/login.tsx
+import LoginPage from '../../components/bid_build/LoginPage'; // Adjust path based on your folder structure
+
+const LoginPageWrapper = () => {
+  return <LoginPage />;
+};
+
+export default LoginPageWrapper;
