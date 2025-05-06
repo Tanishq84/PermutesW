9ad32532407b838bc7dfb93ee63f781654f2086c
Akshat Bisht <akshatyogeshbisht@gmail.com>
2025-05-07 01:36:52 +0530

    3 JS issue solved & Protected routing added plus file restructuring with catch all

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 // pages/dashboard.tsx
 import { NextPage } from 'next';
-import Dashboard from '../components/bid_build/dashboard';
+import Dashboard from '../../components/bid_build/dashboard';
 
 const DashboardPage: NextPage = () => {
   return (
